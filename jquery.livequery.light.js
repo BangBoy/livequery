75f69a4422e94fc6de1d90d40b158d584c200ca6
Willy Hung <willy.hung@armorize.com>
2012-04-12 15:17:58 +0800

    For Personal Use: As I only use livequery for DOMNodeInserted issue.. Remove other parts I do not need.

---
+++ jquery.livequery.light.js
@@ -0,0 +1,144 @@
+/*! Copyright (c) 2010 Brandon Aaron (http://brandonaaron.net)
+ * Dual licensed under the MIT (MIT_LICENSE.txt)
+ * and GPL Version 2 (GPL_LICENSE.txt) licenses.
+ *
+ * Version: 1.2(light)
+ * Requires jQuery 1.3+
+ * Docs: http://docs.jquery.com/Plugins/livequery
+ */
+
+(function($) {
+
+$.extend($.fn, {
+	livequery: function(fn) {
+		var self = this, q;
+
+		// See if Live Query already exists
+		$.each( $.livequery.queries, function(i, query) {
+			if ( self.selector == query.selector && self.context == query.context &&
+				(!fn || fn.$lqguid == query.fn.$lqguid))
+					// Found the query, exit the each loop
+					return (q = query) && false;
+		});
+
+		// Create new Live Query if it wasn't found
+		q = q || new $.livequery(this.selector, this.context, fn);		
+
+		// Run it immediately for the first time
+		q.run();
+
+		// Contnue the chain
+		return this;
+	}
+});
+
+$.livequery = function(selector, context, fn) {
+	this.selector = selector;
+	this.context  = context;
+	this.fn       = fn;	
+	this.elements = [];
+	
+
+	// The id is the index of the Live Query in $.livequery.queries
+	this.id = $.livequery.queries.push(this)-1;
+
+	// Mark the functions for matching later on
+	fn.$lqguid = fn.$lqguid || $.livequery.guid++;
+	
+	// Return the Live Query
+	return this;
+};
+
+$.livequery.prototype = {
+	
+	run: function() {		
+		var query = this;
+
+		var oEls = this.elements,
+			els  = $(this.selector, this.context),
+			nEls = els.not(oEls);
+
+		// Set elements to the latest set of matched elements
+		this.elements = els;
+
+		
+		// Call the first function for newly matched elements
+		nEls.each(function() {
+			query.fn.apply(this);
+		});
+	}
+};
+
+$.extend($.livequery, {
+	guid: 0,
+	queries: [],
+	queue: [],
+	running: false,
+	timeout: null,
+
+	checkQueue: function() {
+		if ( $.livequery.running && $.livequery.queue.length ) {
+			var length = $.livequery.queue.length;
+			// Run each Live Query currently in the queue
+			while ( length-- )
+				$.livequery.queries[ $.livequery.queue.shift() ].run();
+		}
+	},
+	
+	play: function() {
+		// Restart Live Queries
+		$.livequery.running = true;
+		// Request a run of the Live Queries
+		$.livequery.run();
+	},
+
+	registerPlugin: function() {
+		$.each( arguments, function(i,n) {
+			// Short-circuit if the method doesn't exist
+			if (!$.fn[n]) return;
+
+			// Save a reference to the original method
+			var old = $.fn[n];
+
+			// Create a new method
+			$.fn[n] = function() {
+				// Call the original method
+				var r = old.apply(this, arguments);
+
+				// Request a run of the Live Queries
+				$.livequery.run();
+
+				// Return the original methods result
+				return r;
+			}
+		});
+	},
+
+	run: function(id) {
+		if (id != undefined) {
+			// Put the particular Live Query in the queue if it doesn't already exist
+			if ( $.inArray(id, $.livequery.queue) < 0 )
+				$.livequery.queue.push( id );
+		}
+		else
+			// Put each Live Query in the queue if it doesn't already exist
+			$.each( $.livequery.queries, function(id) {
+				if ( $.inArray(id, $.livequery.queue) < 0 )
+					$.livequery.queue.push( id );
+			});
+
+		// Clear timeout if it already exists
+		if ($.livequery.timeout) clearTimeout($.livequery.timeout);
+		// Create a timeout to check the queue and actually run the Live Queries
+		$.livequery.timeout = setTimeout($.livequery.checkQueue, 20);
+	}
+});
+
+// Register core DOM manipulation methods
+$.livequery.registerPlugin('append', 'prepend', 'after', 'before', 'wrap', 'attr', 'removeAttr', 'addClass', 'removeClass', 'toggleClass', 'empty', 'remove', 'html', 'prop', 'removeProp');
+//$.livequery.registerPlugin('html'); //
+
+// Run Live Queries when the Document is ready
+$(function() { $.livequery.play(); });
+
+})(jQuery);
